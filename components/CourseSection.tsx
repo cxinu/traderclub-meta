61650575590aa93f6b871c2d33f0091813cf8a99
cxinu <72374438+cxinu@users.noreply.github.com>
2024-07-20 03:48:22 +0530

    course pricing

--- components/CourseSection.tsx
+++ components/CourseSection.tsx
@@ -1,46 +1,84 @@
+function TickMark({ color }: { color?: string }) {
+  return (
+    <svg
+      width="29"
+      height="22"
+      viewBox="0 0 29 22"
+      fill="none"
+      className="check-icon"
+      data-astro-cid-j7pv25f6=""
+    >
+      <path
+        d="M3 9.72308L11.1385 17.8615L26 3"
+        {...(color ? { stroke: color } : { stroke: "var(--accent)" })}
+        stroke-width="5.66154"
+        data-astro-cid-j7pv25f6=""
+      ></path>
+    </svg>
+  );
+}
+
+function ListItem({ p, color }: { p: string; color?: string }) {
+  return (
+    <div className="flex gap-4">
+      <TickMark color={color} />
+      <p>{p}</p>
+    </div>
+  );
+}
 export default function CourseSection() {
   return (
     <div className="bg-slate-100">
       <section id="courses" className="course container section">
         <h1 className="course__title">
-          {" "}
           <strong>Courses ✦</strong> We Offer
         </h1>
         <div className="course__wrapper">
           <div className="course__card">
-            <h2>BASIC COURSE</h2>
-            <p>
-              Stock market investments is designed to make basic course on stock
-              market you understand complete basics on stock markets. Anyone who
-              can understand minimum English / Hindi can attend this course and
-              learn. After this basic course on stock market investment you will
-              feel very confident to invest in equity markets. You will feel
-              extremely overwhelmed after the session .We cover fundamental
-              analysis and technical analysis also.
-            </p>
+            <div>
+              <h2>BASIC COURSE</h2>
+              <div className="flex flex-col gap-6 mt-4">
+                <ListItem p="Fundamental Analysis" />
+                <ListItem p="Technical Analysis" />
+              </div>
+            </div>
+            <h2 className="text-slate-600">₹14,999</h2>
           </div>
           <div className="course__card best-offer">
-            <h2>GOLD MEMBER PACKAGE</h2>
-            <p>
-              The main intention of advanced technical course is to educate
-              students all the core Technicals in stock markets history. We will
-              train you in such a way that you can trade independently and make
-              money consistently. We teach advanced technical course in a right
-              way. We also provide live trading which other institutions won’t
-              provide at all.
-            </p>
+            <div>
+              <h2>GOLD MEMBER PACKAGE</h2>
+              <div className="flex flex-col gap-6 mt-4">
+                <ListItem p="Fundamental Analysis" color="var(--background)" />
+                <ListItem p="Technical Analysis" color="var(--background)" />
+                <ListItem
+                  p="Advance Technical Analysis"
+                  color="var(--background)"
+                />
+                <ListItem p="Intraday Stocks Tips" color="var(--background)" />
+                <ListItem p="Live Trading Hours" color="var(--background)" />
+              </div>
+            </div>
+            <h2 className="flex justify-center">₹24,999</h2>
           </div>
           <div className="course__card">
-            <h2>ELITE TRADERS PROGRAM</h2>
-            <p>
-              Start your trading with Trader Club during the live hours i.e,
-              9:15 am- 3:30 pm. Know the techniques and intraday stock tips to
-              protect your capital. We have designed a curriculum with a
-              combination of all market segments, including technical, advanced
-              and fundamental analysis tools, for beginners, intermediates, and
-              expert traders.
-            </p>
+            <div>
+              <h2>ELITE TRADERS PROGRAM</h2>
+              <div className="flex flex-col gap-6 mt-4">
+                <ListItem p="Fundamental Analysis" />
+                <ListItem p="Technical Analysis" />
+                <ListItem p="Advance Technical Analysis" />
+                <ListItem p="Intraday Stocks Tips" />
+                <ListItem p="Live Trading Hours" />
+                <ListItem p="Live Mentorship Program" />
+              </div>
+            </div>
+            <h2 className="text-slate-600">₹29,999</h2>
           </div>
+        </div>
+        <div className="px-[16px] w-full sm:w-auto">
+          <button className="btn btn--primary justify-center w-full sm:w-auto">
+            Learn More
+          </button>
         </div>
       </section>
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer__bar">
-        ©2024 - 2025. Traderclub. All Rights Reserved.
+    <footer className="footer bg-slate-200 p-4">
+      <div className="footer__content container flex justify-between">
+        <div className="">
+          <div className="header__logo">
+            <div className="header__image">
+              <Image src="/logo-dark.png" alt="logo" fill={true} />
+            </div>
+            <h4 className="header__text">TRADER CLUB</h4>
+          </div>
+        </div>
+        <p className="text-base h-14 flex flex-col justify-end">
+          ©2024 Traderclub All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
